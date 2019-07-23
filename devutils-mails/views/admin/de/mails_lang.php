7338c657cf48a6a5257fffacdbcf3066a3c4aa92
Sascha Weidner <=>
2019-07-23 12:00:35 +0200

    Prepare metapackage

--- devutils-mails/views/admin/de/mails_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$sLangName = 'Deutsch';
-$aLang = array(
-    'charset'     => 'UTF-8',
-    'dev_mails' => 'Mails Preview'
-);
--- devutils-metadata/views/admin/de/modules_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$sLangName = 'Deutsch';
-$aLang = array(
-    'charset'            => 'UTF-8',
-    'dev_metadata'       => 'Metadata',
-    'devmodule_metadata' => 'Metadata'
-);
